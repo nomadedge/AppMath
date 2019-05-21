f63455e955559b54b019424b1ebf0ecfeab73004
nomadedge <nomad.edge@gmail.com>
2019-05-21 21:22:15 +0300

    task 1 complete

--- project/project.py
+++ project/project.py
@@ -3,22 +3,24 @@ import matplotlib.pyplot as pyplot
 
 
 class task1(object):
-    def __init__(self, learning_rate = 0.1):
-        self.weights_0_1 = numpy.random.normal(0.0, 1, (40, 15))
-        self.weights_1_2 = numpy.random.normal(0.0, 1, (1, 40))
+    def __init__(self, learning_rate=0.1):
+        self.weights_0_1 = numpy.random.random_sample((100, 2)) * 2 - 1
+        self.weights_1_2 = numpy.random.random_sample((1, 100)) * 2 - 1
         self.sigmoid_mapper = numpy.vectorize(self.sigmoid)
         self.lin_mapper = numpy.vectorize(self.lin)
         self.learning_rate = numpy.array([learning_rate])
 
     def func1(self, x):
-        return(numpy.exp(numpy.arctan(x)))
+        return (numpy.exp(numpy.arctan(x)))
         #return(numpy.log(1 + numpy.exp(x)))
+        #return(numpy.exp(x)/numpy.cos(x))
+        #return(numpy.log(1 + numpy.sin(x)))
 
     def sigmoid(self, x):
-        return(1 / (1 + numpy.exp(-x)))
-    
+        return (1 / (1 + numpy.exp(-x)))
+
     def lin(self, x):
-        return(x)
+        return (x)
 
     def predict(self, inumpyuts):
         inumpyuts_1 = numpy.dot(self.weights_0_1, inumpyuts)
@@ -35,27 +37,30 @@ class task1(object):
         actual_predict = outputs_2
 
         error_layer_2 = numpy.array([actual_predict - expected_predict])
-        gradient_layer_2 = numpy.ones(len(actual_predict))# * (1 - actual_predict)
+        gradient_layer_2 = numpy.ones(len(actual_predict))  # * (1 - actual_predict)
         weights_delta_layer_2 = error_layer_2 * gradient_layer_2
-        self.weights_1_2 -= (numpy.dot(weights_delta_layer_2, outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
+        self.weights_1_2 -= (numpy.dot(weights_delta_layer_2,
+                                       outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
 
         error_layer_1 = weights_delta_layer_2 * self.weights_1_2
         gradient_layer_1 = outputs_1 * (1 - outputs_1)
         weights_delta_layer_1 = error_layer_1 * gradient_layer_1
-        self.weights_0_1 -= numpy.dot(inumpyuts.reshape(len(inumpyuts), 1), weights_delta_layer_1).T  * self.learning_rate
+        self.weights_0_1 -= numpy.dot(inumpyuts.reshape(len(inumpyuts), 1),
+                                      weights_delta_layer_1).T * self.learning_rate
+
 
 class task2(object):
-    def __init__(self, learning_rate = 0.1):
+    def __init__(self, learning_rate=0.1):
         self.weights_0_1 = numpy.random.normal(0.0, 1, (3, 3))
         self.weights_1_2 = numpy.random.normal(0.0, 1, (1, 3))
         self.sigmoid_mapper = numpy.vectorize(self.sigmoid)
         self.learning_rate = numpy.array([learning_rate])
 
     def func2(self, x, y, z):
-        return((not(x or y)) == (not(z) or x))
+        return ((not (x or y)) == (not (z) or x))
 
     def sigmoid(self, x):
-        return(1 / (1 + numpy.exp(-x)))
+        return (1 / (1 + numpy.exp(-x)))
 
     def predict(self, inumpyuts):
         inumpyuts_1 = numpy.dot(self.weights_0_1, inumpyuts)
@@ -75,26 +80,28 @@ class task2(object):
         gradient_layer_2 = actual_predict * (1 - actual_predict)
         weights_delta_layer_2 = error_layer_2 * gradient_layer_2
         self.weights_1_2 -= (numpy.dot(weights_delta_layer_2,
-            outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
+                                       outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
 
         error_layer_1 = weights_delta_layer_2 * self.weights_1_2
         gradient_layer_1 = outputs_1 * (1 - outputs_1)
         weights_delta_layer_1 = error_layer_1 * gradient_layer_1
         self.weights_0_1 -= numpy.dot(inumpyuts.reshape(len(inumpyuts), 1),
-            weights_delta_layer_1).T  * self.learning_rate
+                                      weights_delta_layer_1).T * self.learning_rate
+
 
 def MSE(arr1, arr2):
-    return(numpy.mean((arr1 - arr2) ** 2))
+    return (numpy.mean((arr1 - arr2) ** 2))
+
 
 train1 = [[]]
 leftBorder = -10
 rightBorder = 10
-n = 2000
+n = 1000
 x = numpy.linspace(leftBorder, rightBorder, n)
 epochs = 2000
-learning_rate = 0.00001
+learning_rate = 0.00045
 loss = []
-t1 = task1(learning_rate = learning_rate)
+t1 = task1(learning_rate=learning_rate)
 for val in x:
     f = t1.func1(val)
     buf = []
@@ -110,28 +117,54 @@ for e in range(epochs):
     correct_predictions = []
     for d in train1:
         inp = []
-        for i in range(0, 15):
-            inp.append(d[0])
+        inp.append(d[0])
+        inp.append(1)
         t1.train(numpy.array(inp), d[1])
         inumpyuts_.append(numpy.array(inp))
         correct_predictions.append(numpy.array(d[1]))
-    train_loss = MSE(t1.predict(numpy.array(inumpyuts_).T), numpy.array(correct_predictions))
-    loss.append(train_loss)
-
-for d in range(0, 100):
-    h = numpy.random.random_sample() * 20 - 10
-    inp = []
-    for i in range(0, 15):
+    # a = numpy.random.random_sample() * 20 - 10
+    # inp = []
+    # inp.append(a)
+    # inp.append(1)
+    # b = t1.func1(a)
+    # t1.train(numpy.array(inp), b)
+    # inumpyuts_.append(numpy.array(inp))
+    # correct_predictions.append(numpy.array(b))
+    eps = 0.02
+    n = 0
+    ma = 0
+    for d in range(0, 1000):
+        h = numpy.random.random_sample() * 20 - 10
+        inp = []
         inp.append(h)
-    print(h, '\t', t1.predict(numpy.array(inp)), '\t', t1.func1(h))
+        inp.append(1)
+        di = numpy.abs(t1.predict(numpy.array(inp)) - t1.func1(h))
+        if (di < eps):
+            n += 1
+        if (di > ma):
+            ma = di
+    print(n / 1000)
+    print(ma)
+    #train_loss = MSE(t1.predict(numpy.array(inumpyuts_).T), numpy.array(correct_predictions))
+    loss.append(ma)
 
-
+# eps = 0.02
+# n = 0
+# for d in range(0, 10000):
+#     h = numpy.random.random_sample() * 20 - 10
+#     inp = []
+#     inp.append(h)
+#     inp.append(1)
+#     print(h, '\t', t1.predict(numpy.array(inp)), '\t', t1.func1(h))
+#     if (numpy.abs(t1.predict(numpy.array(inp)) - t1.func1(h)) < eps):
+#         n += 1
+# print(n / 10000)
 pyplot.plot(loss)
 pyplot.ylabel('MSE')
 pyplot.xlabel('epoch')
 pyplot.show()
 
-#train2 = [
+# train2 = [
 #    ([0, 0, 0], 1),
 #    ([0, 0, 1], 0),
 #    ([0, 1, 0], 0),
@@ -141,13 +174,13 @@ pyplot.show()
 #    ([1, 1, 0], 0),
 #    ([1, 1, 1], 0),
 #    ]
-#epochs = 7000
-#learning_rate = 0.12
-#loss = []
+# epochs = 7000
+# learning_rate = 0.12
+# loss = []
 
-#t2 = task2(learning_rate = learning_rate)
+# t2 = task2(learning_rate = learning_rate)
 
-#for e in range(epochs):
+# for e in range(epochs):
 #    inumpyuts_ = []
 #    correct_predictions = []
 #    for inumpyut_stat, correct_predict in train2:
@@ -157,21 +190,21 @@ pyplot.show()
 #    train_loss = MSE(t2.predict(numpy.array(inumpyuts_).T), numpy.array(correct_predictions))
 #    loss.append(train_loss)
 
-#for inumpyut_stat, correct_predict in train2:
+# for inumpyut_stat, correct_predict in train2:
 #    print("For inumpyut: {} the prediction is: {}, expected: {}".format(
 #        str(inumpyut_stat),
 #        str(t2.predict(numpy.array(inumpyut_stat)) > .5),
 #        str(correct_predict == 1)))
 
-#print()
+# print()
 
-#for inumpyut_stat, correct_predict in train2:
+# for inumpyut_stat, correct_predict in train2:
 #    print("For inumpyut: {} the prediction is: {}, expected: {}".format(
 #        str(inumpyut_stat),
 #        str(t2.predict(numpy.array(inumpyut_stat))),
 #        str(correct_predict == 1)))
 
-#pyplot.plot(loss)
-#pyplot.ylabel('MSE')
-#pyplot.xlabel('epoch')
-#pyplot.show()
+# pyplot.plot(loss)
+# pyplot.ylabel('MSE')
+# pyplot.xlabel('epoch')
+# pyplot.show()
